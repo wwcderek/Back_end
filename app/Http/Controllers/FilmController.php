294654fb3e560dbfc3203847741aa3251eada274
Wong Wai Chun <derek@yedpay.com>
2018-01-02 00:59:00 +0800

    filter

--- app/Http/Controllers/FilmController.php
+++ app/Http/Controllers/FilmController.php
@@ -65,8 +65,8 @@ class FilmController extends Controller
 
     public function specificFilms(Request $request)
     {
-        $record = DB::table('film_genre')->join('films', 'film_genre.film_id', '=', 'films.film_id')->where('genre_id', '=', 1)->get();
-        return json_encode($request->category);
+        $record = DB::table('film_genre')->join('films', 'film_genre.film_id', '=', 'films.film_id')->where('genre_id', '=', $request->category)->get();
+        return json_encode($record);
     }
 
 
