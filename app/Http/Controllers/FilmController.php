2e4253430114ab37c4da8bdefce67352300af3d7
Wong Wai Chun <derek@yedpay.com>
2018-01-02 22:50:12 +0800

    add search film function

--- app/Http/Controllers/FilmController.php
+++ app/Http/Controllers/FilmController.php
@@ -72,8 +72,7 @@ class FilmController extends Controller
     public function search(Request $request)
     {
         $filmName = $request->keyword;
-        $condition = ['title'=> $filmName];
-        $record = Film::where($condition)->get();
+        $record = Film::where('title', '=', $filmName)->get();
         if($record)
             return json_encode($record);
         return json_encode(false);
