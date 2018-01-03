b48de2a9835443a1750bf99cef0dc72b65d60ed9
Wong Wai Chun <derek@yedpay.com>
2018-01-03 18:01:05 +0800

    double join

--- app/Http/Controllers/FilmController.php
+++ app/Http/Controllers/FilmController.php
@@ -97,8 +97,8 @@ class FilmController extends Controller
 //            ->join('follows', 'follows.user_id', '=', 'users.id')
 //            ->where('follows.follower_id', '=', 3)
 //            ->get();
-        //return json_encode($record);
-        return $record[0];
+        return json_encode($record);
+        //return $record;
 //        $url =  Storage::url('2017-11-12-14-47-21.png');
 //        return "<img src='".$url."'/>";
     }
