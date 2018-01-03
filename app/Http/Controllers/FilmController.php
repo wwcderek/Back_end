f814c0d57367976f227bb9c6142182b10002f360
Wong Wai Chun <derek@yedpay.com>
2018-01-03 22:45:46 +0800

    double join

--- app/Http/Controllers/FilmController.php
+++ app/Http/Controllers/FilmController.php
@@ -85,7 +85,7 @@ class FilmController extends Controller
     {
         $record = DB::table('films')
 //            ->select('films.title' ,DB::raw("group_concat(role_has_film.role_id)"))
-            ->select('films.title', 'films.description',DB::raw("(group_concat(roles.name SEPARATOR ',')) as 'role_name'"))
+            ->select('films.title',DB::raw("(group_concat(roles.name SEPARATOR ' ')) as 'role_name'"))
             ->groupBy('films.title')
             ->join('film_genre', 'film_genre.film_id', '=', 'films.film_id')
             ->join('role_has_film', 'role_has_film.film_id', '=', 'films.film_id')
