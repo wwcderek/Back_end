031b10c6bcdc965f0e748ef8d0e7639b5a8ffb4a
Wong Wai Chun <derek@yedpay.com>
2018-01-03 16:17:26 +0800

    double join

--- app/Http/Controllers/FilmController.php
+++ app/Http/Controllers/FilmController.php
@@ -83,11 +83,12 @@ class FilmController extends Controller
 
     public function show()
     {
-        $record = DB::table('film')->join('film_genre', 'film_genre.film_id', '=', 'films.film_id')
-            ->join('role_has_film', 'role_has_film.film_id', '=', 'film.film_id')
-            ->where('genre_id', '=', 1)
-            ->get();
-        return json_encode($record);
+//        $record = DB::table('film')->join('film_genre', 'film_genre.film_id', '=', 'films.film_id')
+//            ->join('role_has_film', 'role_has_film.film_id', '=', 'film.film_id')
+//            ->where('genre_id', '=', 1)
+//            ->get();
+       // return json_encode($record);
+        return "see";
 //        $url =  Storage::url('2017-11-12-14-47-21.png');
 //        return "<img src='".$url."'/>";
     }
