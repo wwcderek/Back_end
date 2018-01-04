07e21db406e9448e5c1c292b539b3b5ebfd82e89
Wong Wai Chun <derek@yedpay.com>
2018-01-04 17:52:23 +0800

    test

--- app/Http/Controllers/FilmController.php
+++ app/Http/Controllers/FilmController.php
@@ -126,7 +126,8 @@ class FilmController extends Controller
         //return json_encode($record);
         $film = Film::where('title', '=','Testing2')->first();
         foreach ($film->roles() as $fi)
-        return json_encode($fi->name);
+            echo $fi->name . ' ' . $fi->type;
+
 
 
     }
