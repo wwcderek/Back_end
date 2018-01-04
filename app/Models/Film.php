fa60e571fb9432080584a78182523434f0a944d3
Wong Wai Chun <derek@yedpay.com>
2018-01-04 15:16:04 +0800

    test

--- app/Models/Film.php
+++ app/Models/Film.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Film extends Model
+class Film extends Eloquent
 {
     //
     protected $table = 'films';
--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,12 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Role extends Model
+class Role extends Eloquent
 {
     //
     protected $table = 'roles';
     public $timestamps = false;
-    
+
     public function film() {
        return $this->belongsToMany('Film', 'role_has_film', 'role_id', 'film_id');
     }
