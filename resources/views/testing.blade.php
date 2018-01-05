348b8fe46ad42a3ea87c7f7b84af79e4f0e9117f
Wong Wai Chun <derek@yedpay.com>
2018-01-05 11:37:47 +0800

    qr-code page

--- resources/views/testing.blade.php
+++ resources/views/testing.blade.php
@@ -1,1 +1,1 @@
-{!! QrCode::generate('Make me into a QrCode!'); !!}
+<img src="data:image/png;base64, {{ base64_encode(QrCode::encoding('UTF-8')->format('png')->margin(1)->size(220)->generate('0023|m123|project|menu')) }}">
