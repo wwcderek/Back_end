200a5dee5d09fb72eb48c6b7710eca93ab036c42
Wong Wai Chun <derek@yedpay.com>
2018-01-05 12:26:22 +0800

    add file

--- resources/views/testing.blade.php
+++ resources/views/testing.blade.php
@@ -1,1 +1,2 @@
-<img src="data:image/png;base64, {{ base64_encode(QrCode::encoding('UTF-8')->format('png')->margin(1)->size(220)->generate('0023|m123|project|menu')) }}">
+{!! QrCode::size(200)->generate('test'); !!}
+
