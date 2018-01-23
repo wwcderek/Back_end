3e5caf5a377d762b2f8d38cec979dc701f4264f6
Wong Wai Chun <derek@yedpay.com>
2018-01-23 09:51:39 +0800

    add QR Code

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,6 +81,11 @@
                 <div class="title m-b-md">
                     Laravel
                 </div>
+                <div class="visible-print text-center">
+                    {!! QrCode::size(100)->generate(Request::url()); !!}
+                    <p>Scan me to return to the original page.</p>
+                </div>
+
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
 //User
 Route::post('/uploadIcon', 'UserController@uploadIcon');
 Route::post('/updateProfile', 'UserController@updateProfile');
