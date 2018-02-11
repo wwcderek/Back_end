20f08a79652949e9bada96df7f5d67153a76d984
Wong Wai Chun <derek@yedpay.com>
2018-02-11 13:44:54 +0800

    add relationship

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -62,7 +62,7 @@ class EventController extends Controller
     {
         //
         $record = DB::table('events')
-            ->select('events.title', 'events.description','events.quota', 'events.event_start_date', 'films.path', 'films.title')
+            ->select('events.title AS eventTitle', 'events.description','events.quota', 'events.event_start_date', 'films.path', 'films.title AS filmTitle')
             ->join('films', 'films.film_id', '=' ,'events.film_id')
             ->orderBy('event_start_date', 'desc')
             ->get();
