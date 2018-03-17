70ab87799b69d0c7187fb7d8ff98c0867b849542
Wong Wai Chun <derek@yedpay.com>
2018-03-17 23:55:36 +0800

    add modal for edit film

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -5,99 +5,58 @@
     <div class="row">
         <div class="col-md-12">
             {{ $films }}
-            {{--<table class="table">--}}
-                {{--<thead>--}}
-                {{--<tr>--}}
-                    {{--<th>--}}
-                        {{--Title--}}
-                    {{--</th>--}}
-                    {{--<th>--}}
-                        {{--Language--}}
-                    {{--</th>--}}
-                    {{--<th>--}}
-                        {{--Rating--}}
-                    {{--</th>--}}
-                    {{--<th>--}}
-                        {{--Running Time--}}
-                    {{--</th>--}}
-                    {{--<th>--}}
-                        {{--Publish Time--}}
-                    {{--</th>--}}
-                {{--</tr>--}}
-                {{--</thead>--}}
-                {{--<tbody>--}}
-                {{--<tr>--}}
-                    {{--<td>--}}
-                        {{--1--}}
-                    {{--</td>--}}
-                    {{--<td>--}}
-                        {{--TB - Monthly--}}
-                    {{--</td>--}}
-                    {{--<td>--}}
-                        {{--01/04/2012--}}
-                    {{--</td>--}}
-                    {{--<td>--}}
-                        {{--Default--}}
-                    {{--</td>--}}
-                {{--</tr>--}}
-                {{--<tr class="active">--}}
-                    {{--<td>--}}
-                        {{--1--}}
-                    {{--</td>--}}
-                    {{--<td>--}}
-                        {{--TB - Monthly--}}
-                    {{--</td>--}}
-                    {{--<td>--}}
-                        {{--01/04/2012--}}
-                    {{--</td>--}}
-                    {{--<td>--}}
-                        {{--Approved--}}
-                    {{--</td>--}}
-                {{--</tr>--}}
-                {{--<tr class="success">--}}
-                    {{--<td>--}}
-                        {{--2--}}
-                    {{--</td>--}}
-                    {{--<td>--}}
-                        {{--TB - Monthly--}}
-                    {{--</td>--}}
-                    {{--<td>--}}
-                        {{--02/04/2012--}}
-                    {{--</td>--}}
-                    {{--<td>--}}
-                        {{--Declined--}}
-                    {{--</td>--}}
-                {{--</tr>--}}
-                {{--<tr class="warning">--}}
-                    {{--<td>--}}
-                        {{--3--}}
-                    {{--</td>--}}
-                    {{--<td>--}}
-                        {{--TB - Monthly--}}
-                    {{--</td>--}}
-                    {{--<td>--}}
-                        {{--03/04/2012--}}
-                    {{--</td>--}}
-                    {{--<td>--}}
-                        {{--Pending--}}
-                    {{--</td>--}}
-                {{--</tr>--}}
-                {{--<tr class="danger">--}}
-                    {{--<td>--}}
-                        {{--4--}}
-                    {{--</td>--}}
-                    {{--<td>--}}
-                        {{--TB - Monthly--}}
-                    {{--</td>--}}
-                    {{--<td>--}}
-                        {{--04/04/2012--}}
-                    {{--</td>--}}
-                    {{--<td>--}}
-                        {{--Call in to confirm--}}
-                    {{--</td>--}}
-                {{--</tr>--}}
-                {{--</tbody>--}}
-            {{--</table>--}}
+            <table class="table">
+                <thead>
+                <tr>
+                    <th>
+                        Title
+                    </th>
+                    <th>
+                        Language
+                    </th>
+                    <th>
+                        Rating
+                    </th>
+                    <th>
+                        Running Time
+                    </th>
+                    <th>
+                        Publish Time
+                    </th>
+                    <th>
+                        Actions
+                    </th>
+                </tr>
+                </thead>
+                <tbody>
+               @foreach($films as $film)
+                <tr class="active">
+                    <td>
+                        {{ empty($film->title) ? "-" : $film->title }}
+                    </td>
+                    <td>
+                        {{ empty($film->language) ? "-" : $film->language }}
+                    </td>
+                    <td>
+                        {{ empty($film->rating) ? "-" : $film->rating }}
+                    </td>
+                    <td>
+                        {{ empty($film->running_time) ? "-" : $film->running_time." mins" }}
+                    </td>
+                    <td>
+                        {{ empty($film->publish_time) ? "-" : $film->publish_time }}
+                    </td>
+                    <td>
+                        <button
+                                type="button"
+                                class="btn btn-primary"
+                        >Edit
+                        </button>
+                    </td>
+                </tr>
+                   @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
