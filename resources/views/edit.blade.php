43241c7832d2cce8a8d90ff3aa51d6b40c16a261
Wong Wai Chun <derek@yedpay.com>
2018-03-18 18:38:01 +0800

    update film

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -11,34 +11,35 @@
                 <img class="card-img-top" alt="Bootstrap Thumbnail Third" src="{{ $film->path }}"  style="width: 350px;height: 200px;padding:0px; left: 0; right: 0; text-align: center; margin-bottom: 20px; margin-left: 100px; margin-right: 100px" />
                 <form id="update-form" action="{{ action('FilmController@updateFilm') }}" method="POST" style="display: none;" >
                     {{ csrf_field() }}
-                <table class="table table-striped">
-                    <tbody>
-                    <tr>
-                        <td width="30%">Film Title</td>
-                        <td width="70%"><input type="text" class="form-control" name='title' id="title" value="{{ $film->title }}"/></td>
-                    </tr>
-                    <tr>
-                        <td width="30%">Film Language</td>
-                        <td width="70%"><input type="text" class="form-control" id="language" value="{{ $film->language }}"/></td>
-                    </tr>
-                    <tr>
-                        <td width="50%">Film Rating</td>
-                        <td width="70%"><input type="text" class="form-control" id="rating" value="{{ $film->rating }}"/></td>
-                    </tr>
-                    <tr>
-                        <td width="50%">Film Running Time</td>
-                        <td width="70%"><input type="text" class="form-control" id="running" value="{{ $film->running_time }}"/></td>
-                    </tr>
-                    <tr>
-                        <td width="50%">Film Publish Time</td>
-                        <td width="70%"><input type="text" class="form-control" id="publish" value="{{ $film->publish_time }}"/></td>
-                    </tr>
-                    <tr>
-                        <td width="50%">Film Description</td>
-                        <td width="70%"><input type="text" class="form-control" id="description" value="{{ $film->description }}"/></td>
-                    </tr>
-                    </tbody>
-                </table>
+                    <input type="text" class="form-control" name='title' id="title" value="{{ $film->title }}"/>
+                {{--<table class="table table-striped">--}}
+                    {{--<tbody>--}}
+                    {{--<tr>--}}
+                        {{--<td width="30%">Film Title</td>--}}
+                        {{--<td width="70%"><input type="text" class="form-control" name='title' id="title" value="{{ $film->title }}"/></td>--}}
+                    {{--</tr>--}}
+                    {{--<tr>--}}
+                        {{--<td width="30%">Film Language</td>--}}
+                        {{--<td width="70%"><input type="text" class="form-control" id="language" value="{{ $film->language }}"/></td>--}}
+                    {{--</tr>--}}
+                    {{--<tr>--}}
+                        {{--<td width="50%">Film Rating</td>--}}
+                        {{--<td width="70%"><input type="text" class="form-control" id="rating" value="{{ $film->rating }}"/></td>--}}
+                    {{--</tr>--}}
+                    {{--<tr>--}}
+                        {{--<td width="50%">Film Running Time</td>--}}
+                        {{--<td width="70%"><input type="text" class="form-control" id="running" value="{{ $film->running_time }}"/></td>--}}
+                    {{--</tr>--}}
+                    {{--<tr>--}}
+                        {{--<td width="50%">Film Publish Time</td>--}}
+                        {{--<td width="70%"><input type="text" class="form-control" id="publish" value="{{ $film->publish_time }}"/></td>--}}
+                    {{--</tr>--}}
+                    {{--<tr>--}}
+                        {{--<td width="50%">Film Description</td>--}}
+                        {{--<td width="70%"><input type="text" class="form-control" id="description" value="{{ $film->description }}"/></td>--}}
+                    {{--</tr>--}}
+                    {{--</tbody>--}}
+                {{--</table>--}}
                 </form>
             </div>
             <div class="modal-footer">
