f73806861ece5e32183ff6e07b95d085870a0724
Wong Wai Chun <derek@yedpay.com>
2018-03-18 21:48:03 +0800

    add user list

--- public/js/user.js
+++ public/js/user.js
@@ -1,6 +1,6 @@
 function searchUser() {
     var value = document.getElementById("search_value").value.trim();
     var url = 'http://101.78.175.101:6780/userList';
-    url.searchParams.set('username', value);
+    url.isURLSearchParams.set('username', value);
     window.location.assign(url.toString());
 }
