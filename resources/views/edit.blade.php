2c5b9be59c61616d4c9b813818073f88b3978353
Wong Wai Chun <derek@yedpay.com>
2018-03-18 16:53:42 +0800

    add serch bar

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -8,7 +8,7 @@
                 <h4 class="modal-title" id="myModalLabel">Film Information</h4>
             </div>
             <div class="modal-body">
-                <img class="card-img-top" alt="Bootstrap Thumbnail Third" src="{{ $film->path }}"  style="width: 350px;height: 200px;padding:0px; margin:0 auto; left: 0; right: 0; text-align: center; margin-bottom: 20px; margin-left: 120px" />
+                <img class="card-img-top" alt="Bootstrap Thumbnail Third" src="{{ $film->path }}"  style="width: 350px;height: 200px;padding:0px; margin:0 auto; left: 0; right: 0; text-align: center; margin-bottom: 20px; margin-left: 110px" />
                 <table class="table table-striped">
                     <tbody>
                     <tr>
