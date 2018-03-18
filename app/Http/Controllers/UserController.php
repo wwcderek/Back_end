fd4f1b90e834716468042f9577b8eb93aa7ead3a
Wong Wai Chun <derek@yedpay.com>
2018-03-18 22:26:33 +0800

    add user list

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -80,6 +80,6 @@ class UserController extends Controller
                 'email' => request('email'),
                 'role' => request('role')
             ]);
-        return redirect()->route('userList');
+        return redirect()->route('/userList');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', 'LoginController@showLoginForm');
 Route::get('/userList/{username?}', 'UserController@userList');
 Route::post('/uploadIcon', 'UserController@uploadIcon');
 Route::post('/updateProfile', 'UserController@updateProfile');
-Route::post('updateUser', 'UserController@updateUser');
+Route::post('/updateUser', 'UserController@updateUser');
 
 
 //Account
