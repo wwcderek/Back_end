039ec47c6566e6068c4458e638f40537f8276ec0
Wong Wai Chun <derek@yedpay.com>
2018-04-12 11:23:24 +0800

    add script

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -82,15 +82,14 @@
                     </button>
                 </form>
             </div>
-            {{--<div>--}}
-                {{--<h2>Show Image</h2>--}}
-                {{--<img src="{{ asset('storage/publish/upload/') }}"/>--}}
-            {{--</div>--}}
         </div>
     </div>
 @endsection
 
 @section('scripts')
-    <script src="{{ asset('js/app.js') }}"></script>
     <script src="{{ asset('js/upload.js') }}"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
 @endsection
